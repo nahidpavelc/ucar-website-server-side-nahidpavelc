85e5d87c1287e09d776a3a63fcbb1fbe72b68a8f
nahidpavelc <nahidpavelchowdhury777@hotmail.com>
2021-11-19 10:43:26 +0600

    commit update

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const app = express()
 const { MongoClient } = require('mongodb');
-
 const cors = require('cors');
+const admin = require("firebase-admin");
 require('dotenv').config();
 
-const app = express()
+
+//Find Object ID
+const ObjectId = require('mongodb').ObjectId;
+
+
 const port = process.env.PORT || 5000;
+
+// sdk server
+// u - car - firebase - adminsdk.json
+const serviceAccount = require('./u-car-firebase-adminsdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 //middleware
 app.use(cors());
@@ -14,6 +27,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvoog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+        }
+    }
+    next();
+}
 
 async function run() {
     try {
@@ -24,6 +51,36 @@ async function run() {
         const bookingsCollection = database.collection('bookings');
         const userCollection = database.collection('users');
 
+        // GET API Cars 
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find({});
+            const cars = await cursor.toArray();
+            res.json(cars);
+        })
+        //GET Single Car
+        app.get('/cars/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('car id', id);
+            const query = { _id: ObjectId(id) };
+            const car = await carsCollection.findOne(query);
+            res.json(car);
+        })
+
+        // GET API Review
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.json(reviews);
+        })
+        //Post API review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log('hit the post api', review);
+            const result = await reviewsCollection.insertOne(review);
+            console.log(result);
+            res.json(result);
+        })
+
         //POST API Booking
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
@@ -32,7 +89,7 @@ async function run() {
             res.json(result);
         })
         //GET API Booking
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, async (req, res) => {
             const email = req.query.email;
             const query = { email: email }
             const cursor = bookingsCollection.find(query);
@@ -40,6 +97,17 @@ async function run() {
             res.json(booking);
         });
 
+        //GET API users
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
         //POST API users
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -57,30 +125,25 @@ async function run() {
             res.json(result);
         })
         //PUT API for ADMIN
-        app.put('/users/admin', async (req, res) => {
+        app.put('/users/admin', verifyToken, async (req, res) => {
             const user = req.body;
-            console.log('put', user);
-            const filter = { email: user.email };
-            const updateDoc = { $set: { role: 'admin' } };
-            const result = await userCollection.updateOne(filter, updateDoc);
-            res.json(result);
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await userCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email };
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await userCollection.updateOne(filter, updateDoc);
+                    res.json(result);
+                }
+            }
+            else {
+                res.status(403).json({ message: 'you do not have access to make Admin' })
+            }
         })
 
 
-        // GET API Review
-        app.get('/reviews', async (req, res) => {
-            const cursor = reviewsCollection.find({});
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-        })
-        //Post API review
-        app.post('/reviews', async (req, res) => {
-            const review = req.body;
-            console.log('hit the post api', review);
-            const result = await reviewsCollection.insertOne(review);
-            console.log(result);
-            res.json(result);
-        })
+
 
     }
 
